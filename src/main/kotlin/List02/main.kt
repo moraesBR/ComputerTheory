8ac99b2ba533591a0f9379560fd6aaa7ce47d1cb
Albert R. Moraes L <albert.richard@gmail.com>
2021-08-28 19:45:23 -0300

    Question 02 OK!

--- src/main/kotlin/List02/main.kt
+++ src/main/kotlin/List02/main.kt
@@ -1,20 +1,37 @@
-import packages.Question03
-import packages.Question1a
-import packages.Question1b
+import packages.*
 
 fun main(){
     val question1a = Question1a()
     val question1b = Question1b()
+    val question2a = Question2a()
+    val question2b = Question2b()
+    val question2c = Question2c()
+    val question2d = Question2d()
     val question3 = Question03()
 
     val input1a = arrayListOf("0","0100","100","1","1001","010","0001")
     println("\n${question1a.getInfo()}")
     input1a.map { println("$it -> ${question1a.validation(it.toCharArray())}")}
 
-
     val input1b = arrayListOf("b","babab","baab","aab","a","ab","bab","aba")
     println("\n${question1b.getInfo()}")
     input1b.map { println("$it -> ${question1b.validation(it.toCharArray())}")}
+
+    val input2a = arrayListOf("","aa","abacabcca","b")
+    println("\n${question2a.getInfo()}")
+    input2a.map { println("$it -> ${question2a.validation(it.toCharArray())}")}
+
+    val input2b = arrayListOf("aaa","aaabbc","caaa","aaabaaa","caaac")
+    println("\n${question2b.getInfo()}")
+    input2b.map { println("$it -> ${question2b.validation(it.toCharArray())}")}
+
+    val input2c = arrayListOf("a","b","aab","abb","aabb","aa","bb")
+    println("\n${question2c.getInfo()}")
+    input2c.map { println("$it -> ${question2c.validation(it.toCharArray())}")}
+
+    val input2d = arrayListOf("ac","bac","a","aabbac","abc","bc")
+    println("\n${question2d.getInfo()}")
+    input2d.map { println("$it -> ${question2d.validation(it.toCharArray())}")}
 
     val text3 = "O computador é uma máquina capaz de variados tipos de tratamento automático de informações ou " +
             "processamento de dados. Entende-se por computador um sistema físico que realiza algum tipo de " +
--- src/main/kotlin/packages/Question02.kt
+++ src/main/kotlin/packages/Question02.kt
@@ -3,59 +3,127 @@ package packages
 class Question2a (states: Set<String> = setOf("q0","q1","q2","q3"),
                      alphabet: String = "abc",
                      initialState: String = "q0",
-                     finalState: Set<String> = setOf("q1","q2")
+                     finalState: Set<String> = setOf("q0","q1","q2","q3")
                  ) : DFA(states, alphabet, initialState, finalState){
-    override fun delta(state: String, input: Char): String {
-        TODO("Not yet implemented")
-    }
+    override fun delta(state: String, input: Char) =
+        if(alphabet.contains(input) && states.contains(state)){
+            when(state){
+                "q0" -> when (input){ 'a' -> "q1"; else -> "erro"}
+                "q1" -> when (input){ 'a' -> "q1"; 'b' -> "q2"; 'c' -> "q3"; else -> "erro"}
+                "q2" -> when (input){ 'a' -> "q1"; 'b' -> "q2"; 'c' -> "q3"; else -> "erro" }
+                "q3" -> when (input){ 'a' -> "q1"; 'c' -> "q3"; else -> "erro" }
+                else -> "erro"
+            }
+        }
+        else "erro"
 
     override fun getInfo(): String {
-        TODO("Not yet implemented")
+        return "Question 2a: Construa um autômato finito que reconheça as cadeias pertence a linguagem (ab*c*)*"
     }
 
+    fun validation (word: CharArray): Boolean {
+        var currentState = initialState
+        for ( input in word ){
+            currentState = delta(currentState, input)
+            if(!states.contains(currentState)) return false
+        }
+        return if(finalState.contains(currentState)) true else false
+    }
 }
 
-class Question2b (states: Set<String> = setOf("q0","q1","q2","q3","q4","q5","q6","q7","q8"),
+class Question2b (states: Set<String> = setOf("q0","q1","q2","q3","q4","q5","q6","q7"),
                      alphabet: String = "abc",
                      initialState: String = "q0",
-                     finalState: Set<String> = setOf("q8")
+                     finalState: Set<String> = setOf("q3","q7")
                  ) : DFA(states, alphabet, initialState, finalState){
-    override fun delta(state: String, input: Char): String {
-        TODO("Not yet implemented")
-    }
+    override fun delta(state: String, input: Char): String =
+        if(alphabet.contains(input) && states.contains(state)){
+            when(state){
+                "q0" -> when (input){ 'a' -> "q1"; 'b' -> "q4"; 'c' -> "q4"; else -> "erro"}
+                "q1" -> when (input){ 'a' -> "q2"; else -> "erro"}
+                "q2" -> when (input){ 'a' -> "q3"; else -> "erro" }
+                "q3" -> when (input){ 'b' -> "q3"; 'c' -> "q3"; else -> "erro" }
+                "q4" -> when (input){ 'a' -> "q5"; else -> "erro"}
+                "q5" -> when (input){ 'a' -> "q6"; else -> "erro"}
+                "q6" -> when (input){ 'a' -> "q7"; else -> "erro" }
+                else -> "erro"
+            }
+        }
+        else "erro"
 
     override fun getInfo(): String {
-        TODO("Not yet implemented")
+        return "Question 2b: Construa um autômato finito que reconheça as cadeias pertence a linguagem aaa(b|c)* | (b|c)* aaa"
     }
 
+    fun validation (word: CharArray): Boolean {
+        var currentState = initialState
+        for ( input in word ){
+            currentState = delta(currentState, input)
+            if(!states.contains(currentState)) return false
+        }
+        return if(finalState.contains(currentState)) true else false
+    }
 }
 
-class Question2c (states: Set<String> = setOf("q0","q1","q2","q3","q4"),
+class Question2c (states: Set<String> = setOf("e0","e1","e2","e3","e4"),
                      alphabet: String = "ab",
-                     initialState: String = "q0",
-                     finalState: Set<String> = setOf("q1","q2","q3","q4")
+                     initialState: String = "e0",
+                     finalState: Set<String> = setOf("e1","e2","e4")
                  ) : DFA(states, alphabet, initialState, finalState){
-    override fun delta(state: String, input: Char): String {
-        TODO("Not yet implemented")
-    }
+    override fun delta(state: String, input: Char): String =
+        if(alphabet.contains(input) && states.contains(state)){
+            when(state){
+                "e0" -> when (input){ 'a' -> "e1"; 'b' -> "e2"; else -> "erro"}
+                "e1" -> when (input){ 'a' -> "e3"; 'b' -> "e4" else -> "erro"}
+                "e2" -> "erro"
+                "e3" -> when (input){ 'a' -> "e3"; 'b' -> "e2"; else -> "erro" }
+                "e4" -> when (input){ 'b' -> "e4"; else -> "erro" }
+                else -> "erro"
+            }
+        }
+        else "erro"
 
     override fun getInfo(): String {
-        TODO("Not yet implemented")
+        return "Question 2c: Construa um autômato finito que reconheça as cadeias pertence a linguagem a*b|ab*"
     }
 
+    fun validation (word: CharArray): Boolean {
+        var currentState = initialState
+        for ( input in word ){
+            currentState = delta(currentState, input)
+            if(!states.contains(currentState)) return false
+        }
+        return if(finalState.contains(currentState)) true else false
+    }
 }
 
-class Question2d (states: Set<String> = setOf("q0","q1","q2","q3","q4"),
-                     alphabet: String = "01",
-                     initialState: String = "q0",
-                     finalState: Set<String> = setOf("q1","q3","q4")
+class Question2d (states: Set<String> = setOf("e0","e1","e2","e3"),
+                     alphabet: String = "abc",
+                     initialState: String = "e0",
+                     finalState: Set<String> = setOf("e1","e3")
                  ) : DFA(states, alphabet, initialState, finalState){
-    override fun delta(state: String, input: Char): String {
-        TODO("Not yet implemented")
-    }
+    override fun delta(state: String, input: Char): String  =
+        if(alphabet.contains(input) && states.contains(state)){
+            when(state){
+                "e0" -> when (input){ 'a' -> "e1"; 'b' -> "e2"; else -> "erro"}
+                "e1" -> when (input){ 'a' -> "e1"; 'b' -> "e2"; 'c' -> "e3"; else -> "erro"}
+                "e2" -> when (input){ 'a' -> "e3"; 'b' -> "e2"; else -> "erro" }
+                "e3" -> when (input){ 'c' -> "e3"; else -> "erro" }
+                else -> "erro"
+            }
+        }
+        else "erro"
 
     override fun getInfo(): String {
-        TODO("Not yet implemented")
+        return "Question 2d: Construa um autômato finito que reconheça as cadeias pertence a linguagem a*b*(a|ac*)"
     }
 
+    fun validation (word: CharArray): Boolean {
+        var currentState = initialState
+        for ( input in word ){
+            currentState = delta(currentState, input)
+            if(!states.contains(currentState)) return false
+        }
+        return if(finalState.contains(currentState)) true else false
+    }
 }
